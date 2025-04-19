648e4946bcfafd151c34958c0beee0abfb125995
Copel-KazuyaSuzuki <kazuya.suzuki@copel.jp>
2025-04-19 13:31:18 +0900

    案件情報テーブルにDELETE処理を追加

--- src/main/java/copel/sesproductpackage/core/database/SES_AI_T_JOB.java
+++ src/main/java/copel/sesproductpackage/core/database/SES_AI_T_JOB.java
@@ -36,6 +36,10 @@ public class SES_AI_T_JOB extends SES_AI_T_EntityBase {
      * 重複チェック用SQL.
      */
     private final static String CHECK_SQL = "SELECT COUNT(*) FROM SES_AI_T_JOB WHERE raw_content % ? AND similarity(raw_content, ?) > ?";
+    /**
+     * DELETE文.
+     */
+    private final static String DELETE_SQL = "DELETE FROM SES_AI_T_JOB WHERE job_id = ?";
 
     // ================================
     // メンバ
@@ -139,7 +143,12 @@ public class SES_AI_T_JOB extends SES_AI_T_EntityBase {
 
     @Override
     public boolean deleteByPk(Connection connection) throws SQLException {
-        return false;
+        if (connection == null) {
+            return false;
+        }
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL);
+        preparedStatement.setString(1, this.jobId);
+        return preparedStatement.executeUpdate() > 0;
     }
 
     @Override
