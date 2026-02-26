9ecae6e3822e189425d12717538ce249d87ca3e6
Copel-KazuyaSuzuki <kazuya.suzuki@copel.jp>
2026-02-26 16:11:15 +0900

    要員テーブルのINSERTのエラーを解消

--- src/main/java/copel/sesproductpackage/core/database/SES_AI_T_PERSON.java
+++ src/main/java/copel/sesproductpackage/core/database/SES_AI_T_PERSON.java
@@ -29,7 +29,7 @@ public class SES_AI_T_PERSON extends SES_AI_T_EntityBase {
   // ================================
   /** INSERTR文. */
   private static final String INSERT_SQL =
-      "INSERT INTO SES_AI_T_PERSON (person_id, from_group, from_id, from_name, raw_content, content_summary, file_id, file_summary, vector_data, register_date, register_user, ttl) VALUES (?, ?, ?, ?, ?, ?, ?, ?::vector, ?, ?, ?)";
+      "INSERT INTO SES_AI_T_PERSON (person_id, from_group, from_id, from_name, raw_content, content_summary, file_id, file_summary, vector_data, register_date, register_user, ttl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?::vector, ?, ?, ?)";
 
   /** SELECT文. */
   private static final String SELECT_SQL =
