5af6505175aa987458ed851caf8e93251fd79495
Copel-KazuyaSuzuki <kazuya.suzuki@copel.jp>
2026-08-20 12:36:40 +0900

    SQSエンティティ内部でSQSのURLを取得する形に変更

--- src/main/java/copel/sesproductpackage/core/internal/SesAiAssistantWebAppNotifierSqsEntity.java
+++ src/main/java/copel/sesproductpackage/core/internal/SesAiAssistantWebAppNotifierSqsEntity.java
@@ -2,8 +2,11 @@ package copel.sesproductpackage.core.internal;
 
 import com.amazonaws.regions.Regions;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
 import copel.sesproductpackage.core.api.aws.SQSEntityBase;
 import copel.sesproductpackage.core.util.ObjectMapperFactory;
+import copel.sesproductpackage.core.util.Properties;
+import copel.sesproductpackage.core.util.SsmParameterKey;
 
 /**
  * Web Push 通知 SQS 送信エンティティ. SesAiAssistantWebAppNotifier Lambda をトリガーする SQS へメッセージを送信する.
@@ -11,6 +14,9 @@ import copel.sesproductpackage.core.util.ObjectMapperFactory;
  * @author Copel Co., Ltd.
  */
 public class SesAiAssistantWebAppNotifierSqsEntity extends SQSEntityBase {
+  private static final String SQS_QUEUE_URL_WEBAPP_NOTIFIER =
+      Properties.get(SsmParameterKey.NOTIFIER_QUEUE_URL.getKey());
+
   private final SesAiAssistantWebAppNotifierRequestEntity request;
 
   /**
@@ -19,9 +25,8 @@ public class SesAiAssistantWebAppNotifierSqsEntity extends SQSEntityBase {
    * @param queueUrl SQS の URL
    * @param request プッシュ通知リクエスト
    */
-  public SesAiAssistantWebAppNotifierSqsEntity(
-      String queueUrl, SesAiAssistantWebAppNotifierRequestEntity request) {
-    super(Regions.AP_NORTHEAST_1, queueUrl);
+  public SesAiAssistantWebAppNotifierSqsEntity(SesAiAssistantWebAppNotifierRequestEntity request) {
+    super(Regions.AP_NORTHEAST_1, SQS_QUEUE_URL_WEBAPP_NOTIFIER);
     this.request = request;
   }
 
